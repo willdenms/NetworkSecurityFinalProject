599c085e31bf118cf3651c509f615a5be99f7235
Matt Howa <jdbball1@gmail.com>
2016-11-23 19:21:10 -0700

    Successfully created button click event

--- popupTest.js
+++ popupTest.js
@@ -1,8 +1,21 @@
 /**
  * Created by Matt Willden on 11/22/16.
  */
-function clickHandler() {
+//function clickHandler() {
+//    window.webkitNotifications.createNotification("Sherlock.gif", "Clicked", "It was clicked").show();
+//
+//
+//}
+
+function test() {
+    console.log("made it to test");
+    // debugger;
+    //alert("Triggered JS")
+    $("#test").html("I have been clicked");
     window.webkitNotifications.createNotification("Sherlock.gif", "Clicked", "It was clicked").show();
-
-
 }
+
+$(document).ready(function() {
+
+    $("#btn").click(test);
+});

3b7b42252fc1115848908f0370360176cf718ea5
willdenms <willdenms@gmail.com>
2016-12-07 09:23:22 -0700

    Just pushing a few changes, but they are commented out right now

--- js/willden/mediaAlert.js
+++ js/willden/mediaAlert.js
@@ -1,3 +1,27 @@
-/**
- * Created by  Matt Willden on 12/6/16.
- */
+function getDevices() {
+    navigator.mediaDevices.enumerateDevices()
+        .then(function (MediaDeviceInfo) {
+            if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+                console.log("enumerateDevices() not supported.");
+
+
+                return;
+            }
+
+// List cameras and microphones.
+
+            navigator.mediaDevices.enumerateDevices()
+                .then(function (devices) {
+                    devices.forEach(function (device) {
+                        console.log(device.kind + ": " + "label: " + device.label +
+                            " id = " + device.deviceId);
+                        //if(device.label != "De")
+                    });
+                })
+                .catch(function (err) {
+                    console.log(err.name + ": " + err.message);
+                });
+
+        });
+}
+
